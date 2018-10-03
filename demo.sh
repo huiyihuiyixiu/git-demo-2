0871627353d921db49fd17ad0efc0c797ddc50df
huiyihuiyixiu <43297736+huiyihuiyixiu@users.noreply.github.com>
2018-10-03 09:29:43 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@ pwd # 确认一下当前路径是不是 ~/local 或者 /c/Users/你的名字/loc
 mkdir $1
 cd $1
 mkdir css js
-echo "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>" > index.html
+echo "<!DOCTYPE><title>Hello</title><h1>Hi</h1>" > index.html
 echo "h1{color: red;}" > css/style.css
-echo "var string = \"Hello World\"\nalert(string)" > js/main.js
+echo "var string = 'Hello World';alert(string)" > js/main.js
 exit
